92b740ea57ed25da58b83d23edd0b92c96338e39
Florent Jacquet <florent.jacquet@free-electrons.com>
2017-04-25 15:10:06 +0200

    Update report to match the new API function and v2 tests
    Signed-off-by: Florent Jacquet <florent.jacquet@free-electrons.com>

--- report/app.py
+++ report/app.py
@@ -5,26 +5,22 @@
 #
 
 import sys
-import json
+import ruamel.yaml
 from xmlrpc import client
 from flask import Flask, render_template
 from datetime import datetime
 
 
 hostname = "farm"
-server = client.ServerProxy("http://%s/RPC2" % (hostname))
+
+username = "custom-tests"
+token = "my-awesome-token"
+
+server = client.ServerProxy("http://%s:%s@%s/RPC2" % (username, token, hostname))
 
 print(server.system.listMethods())
 
-device_type_blacklist = ['dummy-ssh']
-tests_list = {
-        "network": 'lava-test-shell-run',
-        "mmc": '/tests/tests/mmc',
-        "usb": '/tests/tests/usb',
-        "sata": '/tests/tests/sata',
-        "first_test": '/tests/tests/first_test',
-        }
-
+tests_list = set()
 
 app = Flask(__name__)
 
@@ -54,36 +50,38 @@ def generate():
             }
         }
     """
-    devices = {d[0]:{t:[] for t in tests_list.keys()} for d in server.scheduler.all_devices()}
-    # print(devices)
-    for b in reversed(server.dashboard.bundles('/anonymous/custom-tests/')[-400:]):
-        print('.', end='', flush=True)
-        # print(b)
-        bundle = server.dashboard.get(b["content_sha1"])
-        # print("\Bundle: \n\n")
-        bundle_json = json.loads(bundle['content'])
-        # print(json.dumps(bundle_json, indent=4))
-        for t in bundle_json["test_runs"]:
-            # print(t["attributes"]["target.device_type"])
-            if t["attributes"]["target.device_type"] in device_type_blacklist:
-                continue
-            for r in t["test_results"]:
-                for test,prefix in tests_list.items():
-                    if r["test_case_id"].startswith(prefix):
-                        devices[t["attributes"]["target"]][test].append({
-                            "job_id": b["associated_job"],
-                            "job_name": b["content_filename"].split('--'),
-                            "result": r["result"]
-                            })
-                        # print(json.dumps(bundle_json, indent=2))
-    print('\n', end='')
-    html = render_template("index.jinja", devices=devices, tests=tests_list, datetime=datetime)
+    start_time = datetime.now()
+    devices = {}
+    for device in [i[0] for i in server.scheduler.all_devices()]:
+        devices[device] = {}
+        for job in server.scheduler.get_last_n_runned_jobs_for_device(device, 20):
+            j_id = job["id"]
+            job_name = job['description']
+            print('.', end='', flush=True)
+            try:
+                f = server.results.get_testsuite_results_yaml(j_id, '1_custom-tests')
+                result = ruamel.yaml.load(f, Loader=ruamel.yaml.RoundTripLoader)
+                res_dict = {
+                        "job_id": result[0]['job'],
+                        "job_name": job_name.split("--"),
+                        "result": result[0]['result'],
+                        }
+                try:
+                    devices[device][result[0]['name']].append(res_dict)
+                except:
+                    devices[device][result[0]['name']] = [res_dict]
+                tests_list.add(result[0]['name'])
+            except: pass
+    html = render_template("index.jinja", devices=devices, tests=tests_list,
+            datetime=datetime, start_time=start_time)
     with open("cache.html", 'w') as f:
         f.write(html)
     return html
 
 
+def main():
+    app.run(debug=True, host="0.0.0.0")
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    main()
 
