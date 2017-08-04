9fae202baa980e22de1fc62a3f14aee4261ccf3e
Florent Jacquet <florent.jacquet@free-electrons.com>
2017-08-04 11:45:14 +0200

    notifications: complete refactoring
    Signed-off-by: Florent Jacquet <florent.jacquet@free-electrons.com>

--- notifications.py
+++ notifications.py
@@ -10,19 +10,12 @@ import os
 from xmlrpc import client
 from pprint import pprint
 from datetime import datetime, timedelta
+from collections import OrderedDict
 
 import configparser
 
 import smtplib
 from email.mime.text import MIMEText
-
-hostname = "lava.tld"
-username = "my-user"
-token = "my-awesome-token"
-
-config = configparser.ConfigParser()
-config.read("notifications.conf")
-kwargs = dict(config["mail"])
 
 JOB_STATUS = {
         0: "Submitted",
@@ -33,101 +26,152 @@ JOB_STATUS = {
         5: "Canceling",
         }
 
-def main():
-    hostname = config.get("lava", "hostname")
-    username = config.get("lava", "user")
-    token = config.get("lava", "token")
+JOB_REPORT_STATUS = OrderedDict()
+JOB_REPORT_STATUS['failed_tests'] = "The following jobs did not pass the custom tests:"
+JOB_REPORT_STATUS['failed_boot'] = "The following jobs did not boot:"
+JOB_REPORT_STATUS['lava_error'] = "The following jobs did not reach power up:"
+JOB_REPORT_STATUS['passed_tests'] = "The following jobs have ended up without problem:"
 
-    ctt_root_location = os.path.abspath(os.path.dirname(
-        os.path.realpath(__file__)))
-    with open(os.path.join(ctt_root_location, "ci_tests.json")) as f:
-        boards = json.load(f)
 
-    server = client.ServerProxy("http://%s:%s@%s/RPC2" % (username, token, hostname))
-    end_date = datetime.now()
-    start_date = end_date - timedelta(hours=24)
+# Get config
+config = configparser.ConfigParser()
+config.read("notifications.conf")
 
-    # Get the results
-    complete_tests = server.results.make_custom_query("testjob",
-            "testjob__end_time__gt__%s,"
-            "testjob__end_time__lt__%s,"
-            "testcase__result__exact__Test passed,"
-            "testjob__submitter__exact__custom-tests,"
-            "testsuite__name__exact__1_custom-tests"
-            % (start_date, end_date))
-    incomplete_tests = server.results.make_custom_query("testjob",
-            "testjob__end_time__gt__%s,"
-            "testjob__end_time__lt__%s,"
-            "testjob__submitter__exact__custom-tests,"
-            "testjob__status__exact__Incomplete"
-            % (start_date, end_date))
-    failed_tests = server.results.make_custom_query("testjob",
-            "testjob__end_time__gt__%s,"
-            "testjob__end_time__lt__%s,"
-            "testcase__result__exact__Test failed,"
-            "testjob__submitter__exact__custom-tests,"
-            "testsuite__name__exact__1_custom-tests"
-            % (start_date, end_date))
+# Get board config
+ctt_root_location = os.path.abspath(os.path.dirname(os.path.realpath(__file__)))
+with open(os.path.join(ctt_root_location, "ci_tests.json")) as f:
+    boards_config = json.load(f)
 
-    # Define how to process the jobs lists
-    def append_job(j, test_status):
-        d = j["description"].split("--")
-        # pprint(j)
-        job_report = "".join([
-            '{:<10}'.format(d[1][:7] + '..' if len(d[1]) > 7 else d[1]), # tree
-            '{:<10}'.format(d[2][:7] + '..' if len(d[2]) > 7 else d[2]), # branch
-            '{:<25}'.format(d[0][:22] + '..' if len(d[0]) > 22 else d[0]), # device
-            '{:<30}'.format(d[3][:27] + '..' if len(d[3]) > 27 else d[3]), # defconfig
-            '{:<12}'.format(d[4][:9] + '..' if len(d[4]) > 9 else d[4]), # test
-            '{:<12}'.format(test_status + '..' if len(test_status) > 9 else test_status), # test status
-            '{:<12}'.format(JOB_STATUS[j["status"]]), # job status
-            "http://lava.free-electrons.com/scheduler/job/%s" % j["id"], # link
-            ])
-        board = boards.get(j["requested_device_type_id"], None)
-        if board:
-            for email in board.get("notify", []):
-                if email in mail_list.keys():
-                    mail_list[email].append(job_report)
-                else:
-                    mail_list[email] = [job_report]
+# Get LAVA API handler
+lava_api = client.ServerProxy("http://%s:%s@%s/RPC2" % (
+    config.get("lava", "user"),
+    config.get("lava", "token"),
+    config.get("lava", "hostname")))
 
-    # Process the jobs
-    mail_list = {}
-    for j in failed_tests:
-        append_job(j, "failed")
-    for j in incomplete_tests:
-        append_job(j, "unknown")
-    for j in complete_tests:
-        append_job(j, "OK")
-
-    # Now send the emails
-    server = smtplib.SMTP(kwargs["server"], kwargs["port"])
-    server.ehlo()
-    server.starttls()
-    server.login(kwargs["login"], kwargs["password"])
-    for user,job_list in mail_list.items():
-        msg_list = []
-        msg_list.append("".join([
-            '{:<10}'.format("tree"),
-            '{:<10}'.format("branch"),
+# Define a Job class to ease processing
+class Job(object):
+    TABLE_TITLE = "".join([
+            '{:<12}'.format("tree"),
+            '{:<14}'.format("branch"),
             '{:<25}'.format("device"),
             '{:<30}'.format("defconfig"),
             '{:<12}'.format("test"),
-            '{:<12}'.format("test status"),
-            '{:<12}'.format("job status"),
             "link",
             ])
-        )
-        msg_list += job_list
+
+    def __str__(self):
+        return "".join([
+            '{:<12}'.format(self.tree[:9] + '..' if len(self.tree) > 9 else self.tree),
+            '{:<14}'.format(self.branch[:11] + '..' if len(self.branch) > 11 else self.branch),
+            '{:<25}'.format(self.device[:22] + '..' if len(self.device) > 22 else self.device),
+            '{:<30}'.format(self.defconfig[:27] + '..' if len(self.defconfig) > 27 else self.defconfig),
+            '{:<12}'.format(self.test[:9] + '..' if len(self.test) > 9 else self.test),
+            "http://lava.free-electrons.com/scheduler/job/%s" % self.id,
+            ])
+
+    def __init__(self, job):
+        """
+        `job` is a dictionary returned by the LAVA API
+        """
+        self.id = job['id']
+        d = job['description'].split('--')
+        self.device = d[0]
+        self.tree = d[1]
+        self.branch = d[2]
+        self.defconfig = d[3]
+        self.test = d[4]
+
+    def has_powered_up(self):
+        power_up = lava_api.results.make_custom_query("testjob",
+                "testjob__id__exact__%s,"
+                "testcase__name__exact__pdu-reboot"
+                % (self.id))
+        return bool(power_up)
+
+    def has_booted(self):
+        boot = lava_api.results.make_custom_query("testjob",
+                "testjob__id__exact__%s,"
+                "testcase__name__exact__auto-login-action"
+                % (self.id))
+        return bool(boot)
+
+    def has_passed_test(self):
+        tests = lava_api.results.make_custom_query("testcase",
+                "testjob__id__exact__%s,"
+                "testcase__name__exact__%s"
+                % (self.id, self.test))
+        if not tests:
+            return False
+        return not bool(tests[0]['result'])
+
+
+def main():
+    end_date = datetime.now()
+    start_date = end_date - timedelta(hours=24)
+
+
+    # mail_list is a data structure built like this:
+    # mail_list = {
+    #         'email.address@domain.tld': {
+    #             'passed_tests': [str(Job object), str(other Job object)],
+    #             'failed_tests': [str(Job object), ...],
+    #             }
+    #         }
+    mail_list = {}
+
+    # Get all the last jobs
+    all_jobs = lava_api.results.make_custom_query("testjob",
+            "testjob__end_time__gt__%s,"
+            "testjob__end_time__lt__%s,"
+            "testjob__submitter__exact__custom-tests"
+            % (start_date, end_date))
+
+    # Build mail_list
+    for job in all_jobs:
+        if job['requested_device_type_id'] == "dummy-ssh":
+            continue
+        j = Job(job)
+
+        if j.has_passed_test():
+            status = "passed_tests"
+        elif j.has_booted():
+            status = "failed_tests"
+        elif j.has_powered_up():
+            status = "failed_boot"
+        else:
+            status = "lava_error"
+
+        board = boards_config.get(j.device, None)
+        if board:
+            for email in board.get("notify", []):
+                if email not in mail_list:
+                    mail_list[email] = {}
+                if status not in mail_list[email]:
+                    mail_list[email][status] = [Job.TABLE_TITLE]
+                mail_list[email][status].append(str(j))
+
+
+    # Now send the emails
+    server = smtplib.SMTP(config.get("mail", "server"), config.get("mail", "port"))
+    server.ehlo()
+    server.starttls()
+    server.login(config.get("mail", "login"), config.get("mail", "password"))
+    for user,job_list in mail_list.items():
+
+        msg_list = []
+        for status,msg in JOB_REPORT_STATUS.items():
+            if status in job_list:
+                msg_list += [msg]
+                msg_list += job_list[status]
+                msg_list += [""]
+
         print("\nMessage to %s:" % user)
         print('\n'.join(msg_list))
-        msg = MIMEText(
-                "The following jobs failed during the last 24 hours:\n"
-                "\n" + '\n'.join(msg_list))
+        msg = MIMEText('\n'.join(msg_list))
         msg['Subject'] = "CI summary"
-        msg['From'] = kwargs["mail"]
+        msg['From'] = config.get("mail", "from")
         msg['To'] = user
-        server.sendmail(kwargs["mail"], [user], msg.as_string())
+        server.sendmail(config.get("mail", "from"), [user], msg.as_string())
     server.quit()
     return
 
